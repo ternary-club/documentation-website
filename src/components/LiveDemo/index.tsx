037b2af30d94c14d87fc866c65f81f079666a807
Pedro Campos <pedro@cdias.dev>
2021-11-27 14:37:25 -0300

    Add text editor

--- src/components/LiveDemo/index.tsx
+++ src/components/LiveDemo/index.tsx
@@ -1,18 +1,51 @@
-import React from 'react';
+import React, { useState } from 'react';
+import AceEditor from 'react-ace';
+import 'ace-builds/src-noconflict/mode-terry';
+import 'ace-builds/src-noconflict/theme-kuroir';
+
+import Play from '@site/static/img/play.svg';
 
 import classes from './styles.module.css';
 
-const LiveDemo: React.FC = () => (
-  <div className={classes.container}>
-    <div className={classes.content}>
-      <h1>Live Demo</h1>
-      <div className="row">
-        <div className={classes.code} />
-        <button className={classes.button}>&gt;</button>
-        <div className={classes.code} />
+const LiveDemo: React.FC = () => {
+  const [code, setCode] = useState('');
+  return (
+    <div className={classes.container}>
+      <div className={classes.content}>
+        <h1>Live Demo</h1>
+        <div className="row">
+          <div className={classes.code}>
+            <AceEditor
+              style={{ borderRadius: 10 }}
+              mode="terry"
+              theme="kuroir"
+              name="editor"
+              onChange={(value) => setCode(value)}
+              fontSize={16}
+              width="100%"
+              height="100%"
+              value={code}
+              setOptions={{
+                tabSize: 2,
+                showLineNumbers: true,
+                dragEnabled: true,
+                // firstLineNumber: -10,
+                fontFamily: '"Fira code", "monospace"',
+                scrollPastEnd: true,
+                displayIndentGuides: true,
+                highlightActiveLine: true,
+                showGutter: true,
+                printMargin: true,
+                wrap: true,
+              }}
+            />
+          </div>
+          <button className={classes.button}><Play width={32} height={32} /></button>
+          <div className={classes.code} />
+        </div>
       </div>
     </div>
-  </div>
-);
+  );
+};
 
 export { LiveDemo };
